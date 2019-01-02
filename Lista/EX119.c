efddcb6e74c9020220b69dd3794aaad49bf6eaff
davicarv13 <davicarv13@gmail.com>
2019-01-01 22:00:32 -0200

    Apaga Lista

--- Lista/EX119.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int n, *ptrn;
-	n=5;
-	ptrn=&n;
-	printf("Valor de n a partir de ptrn:%d\n", *ptrn);
-	printf("Endereco de n:%p\n", &n);
-	printf("Valor de ptrn:%p\n", ptrn);
-}
