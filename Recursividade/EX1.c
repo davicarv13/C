6458d5d705e368eaf1feb957092d08b8f0eef7b9
davicarv13 <davicarv13@gmail.com>
2019-01-02 19:18:03 -0200

    Altera EX1.c

--- Recursividade/EX1.c
+++ Recursividade/EX1.c
@@ -14,10 +14,16 @@ int main(){
 }
 
 int calculaPotencia(int x, int n){
-	if(n==1){
-		return x;
+	if(n=0){
+		return 1;
 	}
 	else{
-		return x*calculaPotencia(x, n-1);
+		if(n==1){
+		return x;
 	}
+		else{
+			return x*calculaPotencia(x, n-1);
+		}	
+	}
+	
 }
