afe3e01266e3c8d0c994d00178c404594d63be96
davicarv13 <davicarv13@gmail.com>
2019-01-02 19:12:42 -0200

    EX1.c

---
+++ Recursividade/EX1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calculaPotencia(int x, int n);
+
+int main(){
+	int x, n;
+
+	printf("Informe a base:");
+	scanf(" %d", &x);
+	getchar();
+	printf("Informe a potencia:");
+	scanf(" %d", &n);
+	printf("%d elevado a %d: %d", x, n, calculaPotencia(x, n));
+}
+
+int calculaPotencia(int x, int n){
+	if(n==1){
+		return x;
+	}
+	else{
+		return x*calculaPotencia(x, n-1);
+	}
+}
