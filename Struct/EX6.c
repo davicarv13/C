f21669f064e13a35091b7b5f1ed4c7815fcfd8ba
Davi Carvalho Da Silva <38366263+davicarv13@users.noreply.github.com>
2019-01-26 11:55:36 -0200

    Update EX6.c

--- Struct/EX6.c
+++ Struct/EX6.c
@@ -10,17 +10,20 @@ typedef struct{
 
 typedef struct{
 	char compromisso[60];
-	struct Datas Dt[TAM_AGENDA];
+	Datas data;
 }Agenda;
 
 void preencheAgenda(Agenda *agenda);
+void verificaCompromissos(Agenda *agenda);
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 
-	Agenda *agenda=malloc(TAM_AGENDA*sizeof(Agenda));
+	Agenda agenda[TAM_AGENDA];
 	
 	preencheAgenda(agenda);
+
+	verificaCompromissos(agenda);
 }
 
 void preencheAgenda(Agenda *agenda){
@@ -31,8 +34,50 @@ void preencheAgenda(Agenda *agenda){
 	for(i=0; i<TAM_AGENDA; i++){
 		printf("Informe o compromisso:");
 		scanf("%[^\n]s", agenda[i].compromisso);
+		getchar();
 		printf("Informe o dia:");
-		scanf("%d", agenda->Dt[i].dia);
+		scanf("%d", &agenda[i].data.dia);
+		getchar();
+		printf("Informe o dia:");
+		scanf("%d", &agenda[i].data.mes);
+		getchar();
+		printf("Informe o dia:");
+		scanf("%d", &agenda[i].data.ano);
+		getchar();
 		system("clear || cls");
 	}
+}
+
+void verificaCompromissos(Agenda *agenda){
+	int M, A, i, quant=0;
+
+	system("clear || cls");
+
+	printf("Consulta Agenda:\n");
+
+	do{
+		printf("Informe o mês:");
+		scanf("%d", &M);
+		getchar();
+		printf("Informe o ano:");
+		scanf("%d", &A);
+		getchar();
+
+		quant=0;
+
+		for(i=0; i<TAM_AGENDA; i++){
+			if(M==(agenda[i].data.mes) && A==(agenda[i].data.mes)){
+				quant=1;
+				printf("%-15s \t %s\n", "- Compromisso: ", agenda[i].compromisso);
+				printf("%-15s \t %d:%d:%d \n\n", "- Data: ", agenda[i].data.dia, agenda[i].data.mes, agenda[i].data.ano);
+			}
+		}
+
+		if(quant==0){
+			printf("Nenhum compromisso para esta data!");
+		}
+
+		printf("\n\n");
+
+	}while(M!=0);
 }
