dca0f6fafa65ccc89bbe65c2ea0491577527ecd6
davicarv13 <davicarv13@gmail.com>
2019-02-16 19:16:40 -0200

    Add EX4.c

--- Programacao Estruturada/Struct/Slides/EX4.c
+++ Programacao Estruturada/Struct/Slides/EX4.c
@@ -11,59 +11,57 @@ typedef struct {
 	float p3;
 }Aluno;
 
-void preencheAlunos(Aluno *alunos);
-void verificaAprovadosReprovados(Aluno *alunos);
+void preencheAlunos(Aluno *aluno, int i);
+void verificaAprovadosReprovados(const float *p3, int i);
 
 int main(){
-	Aluno *alunos=malloc(TAM*sizeof(Aluno));
 
-	preencheAlunos(alunos);
-	verificaAprovadosReprovados(alunos);
+	Aluno aluno;
 
-	free(alunos);
-	alunos=NULL;
-}
-
-void preencheAlunos(Aluno *alunos){
-	int i;
-
-	for(i-0; i<TAM; i++){
-		printf("Informe a matricula:");
-		scanf("%d", &alunos[i].matricula);
-		getchar();
-		printf("Informe o primeiro nome:");
-		scanf("%s", alunos[i].nome);
-		getchar();
-		printf("Informe a nota da P1:");
-		scanf("%f", &alunos[i].p1);
-		getchar();
-		printf("Informe a nota da P2:");
-		scanf("%f", &alunos[i].p2);
-		getchar();
-
-		alunos[i].p3=alunos[i].p1+alunos[i].p2;
-
-		system("clear || cls");
-	}
-}
-
-void verificaAprovadosReprovados(Aluno *alunos){
-	int i, aprovados=0, reprovados=0;
+	int i=0;
 
 	for(i=0; i<TAM; i++){
-		if(alunos[i].p3>=6){
-			aprovados++;
-		}
-		else{
-			reprovados++;
-		}
+		preencheAlunos(&aluno, i);
+	}
+}
+
+void preencheAlunos(Aluno *aluno, int i){
+	
+	printf("Informe a matricula:");
+	scanf("%d", &aluno->matricula);
+	getchar();
+	printf("Informe o primeiro nome:");
+	scanf("%s", aluno->nome);
+	getchar();
+	printf("Informe a nota da P1:");
+	scanf("%f", &aluno->p1);
+	getchar();
+	printf("Informe a nota da P2:");
+	scanf("%f", &aluno->p2);
+	getchar();
+
+	aluno->p3=(aluno->p1+aluno->p2)/2;
+
+	verificaAprovadosReprovados(&aluno->p3, i);
+}
+
+void verificaAprovadosReprovados(const float *p3, int i){
+	static int aprovados=0;
+	static int reprovados=0;
+
+	if((*p3)>=6){
+		aprovados++;
+	}
+	else{
+		reprovados++;
 	}
 
-	system("clear || cls");
+	i++;
 
-	printf("Quantidade de alunos aprovados: %d\n", aprovados);
-	printf("Quantidade de alunos reprovados: %d\n", reprovados);
-
-
-
+	if(i==(TAM)){
+		system("clear || cls");
+		printf("Quantidade de alunos aprovados: %d\n", aprovados);
+		printf("Quantidade de alunos reprovados: %d\n", reprovados);
+	}
+	
 }
