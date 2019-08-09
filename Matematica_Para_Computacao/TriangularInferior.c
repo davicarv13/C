db22fb1b72e1d02116b6683b1d793eb75b77139a
Davi Carvalho Da Silva <38366263+davicarv13@users.noreply.github.com>
2019-08-09 11:09:12 -0300

    Update TriangularInferior.c

--- Matematica_Para_Computacao/TriangularInferior.c
+++ Matematica_Para_Computacao/TriangularInferior.c
@@ -1,91 +1,94 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define N 3
+#define TAM 3
 
-void geraMatriz(int M[N][N]);
-void geraVetB(int* B);
-void calculaSolucao(int M[N][N], int *B, float *X);
-void imprimeVetB(int *B);
-void imprimeMatriz(int M[N][N]);
+void geraMatriz(int L[TAM][TAM]);
+void imprimeMatriz(int L[TAM][TAM]);
+void geraVetorB(int *B);
+void imprimeVetorB(int *B);
+void calculaSolucao(int L[TAM][TAM], int *B, float *X);
 void imprimeSolucao(float *X);
 
 int main(){
 	srand((unsigned)time(NULL));
-	
-	int M[N][N], B[N];
-	float X[N];
-	geraMatriz(M);
-	geraVetB(B);
-	calculaSolucao(M, B, X);
-	printf("\nMatriz: \n");
-	imprimeMatriz(M);
-	printf("\nVetor B: \n");
-	imprimeVetB(B);
-	printf("\nSolucao: \n");
+
+	int L[TAM][TAM], B[TAM];
+	float X[TAM];
+
+	geraMatriz(L);
+	imprimeMatriz(L);
+	geraVetorB(B);
+	imprimeVetorB(B);
+	calculaSolucao(L, B, X);
 	imprimeSolucao(X);
 }
 
-void geraMatriz(int M[N][N]){
+void geraMatriz(int L[TAM][TAM]){
 	int i, j;
-	
-	for(i = 0; i < N; i++){
-		for(j = 0; j < N; j++){
-			if(i < j){
-				M[i][j] = 0;
-			}
-			else{
-				M[i][j] = rand()%10+1;
-			}
+
+	for(i = 0; i < TAM; i++){
+		for(j = i+1; j < TAM; j++){
+			L[i][j] = 0;
+		}
+	}
+
+	for(i = 0; i < TAM; i++){
+		for(j = 0; j <= i; j++){
+			L[i][j] = rand()%10+1;
 		}
 	}
 }
-void geraVetB(int* B){
-	int i;
-	
-	for(i = 0; i < N; i++){
-		B[i] = rand()%10+1;		
-	}
-}
 
-void calculaSolucao(int M[N][N], int *B, float *X){
+void imprimeMatriz(int L[TAM][TAM]){
 	int i, j;
-	
-	float soma;
-	
-	for(i = 0; i < N; i++){
-		soma = 0;
-		for(j = 0; j <= i-1; j++){
-			soma += M[i][j] * X[j];
-		}
-		
-		X[i] = (B[i] - soma) / M[i][i];
-	}
-}
-
-void imprimeVetB(int *B){
-	int i;
-	
-	for(i = 0; i < N; i++){
-		printf("%2d ", B[i]);		
-	}
-}
-void imprimeMatriz(int M[N][N]){
-	int i, j;
-	
-	for(i = 0; i < N; i++){
-		for(j = 0; j < N; j++){
-			printf("%2d ", M[i][j]);
+	printf("Matriz L:\n");
+	for(i = 0; i < TAM; i++){
+		for(j = 0; j < TAM; j++){
+			printf("%2d ", L[i][j]);
 		}
 		printf("\n");
 	}
 }
 
+void geraVetorB(int *B){
+	int i;
+
+	for(i = 0; i < TAM; i++){
+		B[i] = rand() % 10+1;
+	}
+}
+void imprimeVetorB(int *B){
+	int i;
+
+	printf("Vetor B\n");
+
+	for(i = 0; i < TAM; i++){
+		printf("%d\n", B[i]);
+	}
+}
+
+void calculaSolucao(int L[TAM][TAM], int *B, float *X){
+	int i, j, k;
+	float soma;
+
+	for(i = 0; i < TAM; i++){
+		soma = 0;
+		for(j = 0; j <= i-1; j++){
+			soma += L[i][j] * X[j];
+		}
+
+		X[i] = (B[i] - soma) / L[i][i];
+	}
+}
+
 void imprimeSolucao(float *X){
 	int i;
-	
-	for(i = 0; i < N; i++){
-		printf("%2.2f ", X[i]);		
+
+	printf("Solucao:\n");
+
+	for(i = 0; i < TAM; i++){
+		printf("%.2f\n", X[i]);
 	}
 }
